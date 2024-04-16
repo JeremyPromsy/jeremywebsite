3b9195d547209dba6a48c241309a5eb63821e55a
JeremyPromsy <jeremypromsy@gmail.com>
2024-04-16 17:13:42 +0200

    Changements

--- JeremFront/src/pages/404.jsx
+++ JeremFront/src/pages/404.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import "../styles/404.css";
+import { Link } from "react-router-dom";
+import "../styles/404.css"
 
-function page404() {
+
+function Page404 () {
     return (
-      <div className="">
-      
-      </div>
-    );
-  }
-  
-export default page404;
+        <div className="p404">
+            <div className="page404">
+                <h1>404</h1>
+                    <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
+            <Link to ="/" className="link">
+                Retournez sur la page d'accueil ! 
+            </Link>
+        </div>
+    )
+}
+
+export default Page404;
