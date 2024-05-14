4fb113ddbb9518947a9766d828fe46b8b985779d
JeremyPromsy <jeremypromsy@gmail.com>
2024-05-14 19:11:51 +0200

    Avancement

--- JeremFront/src/components/Informations.jsx
+++ JeremFront/src/components/Informations.jsx
@@ -18,9 +18,9 @@ function Informations() {
           <p> </p>
         </div>
         <div className="infoLiens">
-          <Link to="https://fr.linkedin.com/in/jeremypromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-linkedin" /></Link>
-          <Link to="https://twitter.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-twitter" /></Link>
-          <Link to="https://github.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-github" /></Link>
+          <Link to="https://fr.linkedin.com/in/jeremypromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-linkedin" className="infoIcons infoIcons1"/></Link>
+          <Link to="https://twitter.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-twitter" className="infoIcons infoIcons2"/></Link>
+          <Link to="https://github.com/JeremyPromsy" target="_blank" rel="noopener"><FontAwesomeIcon icon="fa-brands fa-github" className="infoIcons infoIcons3" /></Link>
         </div>
       </div>
     );
